086bdb34cba90424c58446780827a1aaf5feacf7
Saifeldeen Mohamed <75811385+saifeldeen-bio@users.noreply.github.com>
2024-11-27 21:20:44 +0200

    Update and rename extract_draft.py to extractDraft

--- extract_draft.py
+++
@@ -1,9 +0,0 @@
-import sys
-from Bio import SeqIO
-Fasta=sys.argv[1]
-sampleID=sys.argv[2]
-seq=SeqIO.parse(Fasta,'fasta')
-print(seq)
-allseq=[i for i in SeqIO.parse(Fasta,'fasta')]
-genome=[i for i in allseq if 'RagTag' in i.id]
-SeqIO.write(genome,f'{sampleID}.reordered.fasta','fasta')
